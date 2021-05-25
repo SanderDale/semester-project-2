5f5be37ae139506602187dc9022b30f308ad63ba
SanderDale <57624156+SanderDale@users.noreply.github.com>
2021-05-25 12:22:29 +0200

    applied fixes

--- js/components/displayFeatured.js
+++ js/components/displayFeatured.js
@@ -1,25 +1,24 @@
 export function displayFeatured(featuredToDisplay) {
-    const featuredCardContainer = document.querySelector(".featured-items--card--container");
+	const featuredCardContainer = document.querySelector(".featured-items--card--container");
 
-    featuredCardContainer.innerHTML = "";
+	featuredCardContainer.innerHTML = "";
 
-    const filteredItems = featuredToDisplay.filter(filterItems);
+	const filteredItems = featuredToDisplay.filter(filterItems);
 
-    function filterItems(featuredItems) {
-        if(featuredItems.featured === true) {
-            return true;
-        }
-    }
+	function filterItems(featuredItems) {
+		if (featuredItems.featured) {
+			return true;
+		}
+	}
 
-    filteredItems.forEach(function (items) {
-
-        featuredCardContainer.innerHTML += `<a class="product-card--link" href="details.html?id=${items.id}">
+	filteredItems.forEach(function (items) {
+		featuredCardContainer.innerHTML += `<a class="product-card--link" href="details.html?id=${items.id}">
                                                 <div class="featured-items-card">
                                                     <img src="${items.image_url}">
                                                     <h4>${items.title}</h4>
                                                     <p>${items.description}</p>
                                                     <p>$ ${items.price}</p>
                                                 </div>
-                                            </a>`
-    })
+                                            </a>`;
+	});
 }
--- js/login.js
+++ js/login.js
@@ -1,68 +1,66 @@
 import displayMessage from "./components/displayMessage.js";
 import { saveToken, saveUser } from "./components/userStorage.js";
 import { navSlide, createNav } from "./components/nav.js";
+import { authUrl } from "./constants/data.js";
 
 createNav();
 
 navSlide();
-
 
 const form = document.querySelector("#login--form");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const message = document.querySelector(".message-container");
 
-
 form.addEventListener("submit", submitForm);
 
 function submitForm(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    message.innerHTML = "";
+	message.innerHTML = "";
 
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim();
+	const usernameValue = username.value.trim();
+	const passwordValue = password.value.trim();
 
-    if (usernameValue.length === 0 || passwordValue.length === 0) {
-        return displayMessage("warning", "Please enter a valid username and password", ".message-container");
-    }
+	if (usernameValue.length === 0 || passwordValue.length === 0) {
+		return displayMessage("warning", "Please enter a valid username and password", ".message-container");
+	}
 
-    form.innerHTML = `<div class="loading"></div>`;
+	form.innerHTML = `<div class="loading"></div>`;
 
-    doLogin(usernameValue, passwordValue);
+	doLogin(usernameValue, passwordValue);
 }
 
 async function doLogin(username, password) {
+	const url = authUrl;
 
-    const url = "http://localhost:1337/auth/local";
+	const data = JSON.stringify({ identifier: username, password: password });
 
-    const data = JSON.stringify({identifier: username, password: password});
+	const options = {
+		method: "POST",
+		body: data,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
 
-    const options = {
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
+	try {
+		const response = await fetch(url, options);
+		const json = await response.json();
 
-    try {
-        const response = await fetch(url, options);
-        const json = await response.json();
+		if (json.user) {
+			saveToken(json.jwt);
+			saveUser(json.user);
 
-        if (json.user) {
-            saveToken(json.jwt);
-            saveUser(json.user);
+			location.href = "admin.html";
+		}
 
-            location.href = "admin.html";
-        }
+		if (json.error) {
+			displayMessage("error", "Invalid login details", ".message-container");
+		}
 
-        if (json.error) {
-            displayMessage("error", "Invalid login details", ".message-container");
-        }
-
-        console.log(json);
-    } catch(error) {
-        console.log(error);
-    }
+		console.log(json);
+	} catch (error) {
+		console.log(error);
+	}
 }
